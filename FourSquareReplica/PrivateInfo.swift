331a900b22cbfe775b0c48d1905907bdec3677dc
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-02-08 17:13:30 -0500

    added SecretInfo file for my endpoint information

---
+++ FourSquareReplica/PrivateInfo.swift
@@ -0,0 +1,15 @@
+//
+//  PrivateInfo.swift
+//  FourSquareReplica
+//
+//  Created by Ibraheem rawlinson on 2/8/19.
+//  Copyright © 2019 Stephanie Ramirez. All rights reserved.
+//
+
+import Foundation
+
+struct PrivateInfo {
+    static let clientID = "2BD1GZJW54UDRGLMYQE3KBMIPYFJRUQPDBTGXP4TMQUUKXTV"
+    static let clientSecret = "TYMFPL14E2BBYITDMEZIUJOWWJOL1UQUEIFUGHR150OQHX1H"
+    
+}

5795e349ccb5b6cb10029ffcddc9ef2828fe0a0a
SLRAM <stephier2290@gmail.com>
2019-02-08 12:01:40 -0500

    first commit

---
+++ FourSquareReplica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FourSquareReplica
+//
+//  Created by Stephanie Ramirez on 2/8/19.
+//  Copyright © 2019 Stephanie Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

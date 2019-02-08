10246d1720dadf434b5bcbe9476edc19cacecd37
SLRAM <stephier2290@gmail.com>
2019-02-08 15:19:55 -0500

    basic layout

---
+++ FourSquareReplica/Cells/ListsDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListsDetailTableViewCell.swift
+//  FourSquareReplica
+//
+//  Created by Stephanie Ramirez on 2/8/19.
+//  Copyright © 2019 Stephanie Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ListsDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FourSquareReplica/Views/HomeListView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeListView.swift
+//  FourSquareReplica
+//
+//  Created by Stephanie Ramirez on 2/8/19.
+//  Copyright © 2019 Stephanie Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class HomeListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

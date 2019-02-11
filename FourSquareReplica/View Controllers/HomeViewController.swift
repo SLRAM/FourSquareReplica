1a0dd12e65de30bb3b1ba45c4e88e7715e51a65e
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-02-11 12:47:53 -0500

    added my mapview and contraints to my part of the view

--- FourSquareReplica/View Controllers/HomeViewController.swift
+++ FourSquareReplica/View Controllers/HomeViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  HomeViewController.swift
 //  FourSquareReplica
@@ -9,14 +10,64 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+//Private properties
+    private let homeListView = HomeListView()
+    private let homeMapView = HomeMapView()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        makeSearchBar()
+        mapListButton()
+        setupHomeView()
+        // Do any additional setup after loading the view.
+    }
+    func makeSearchBar() {
+        let searchBar = UISearchBar()
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+    }
+    func mapListButton() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "List", style: .plain, target: self, action: #selector(toggle))
+    }
+    @objc func toggle() {
+        print("pressed toggle")
+        if navigationItem.rightBarButtonItem?.title == "List" {
+            navigationItem.rightBarButtonItem?.title = "Map"
+        } else {
+            navigationItem.rightBarButtonItem?.title = "List"
+        }
+        setupHomeView()
         
     }
-    
+    func setupHomeView() {
+        if navigationItem.rightBarButtonItem?.title == "List" {
+            self.view.addSubview(homeListView)
+            homeListView.myTableView.dataSource = self
+            homeListView.myTableView.delegate = self
+            
+            //self.view.addSubview(quizView)
+            //quizView.myQuizCollectionView.reloadData()
+        } else {
+            self.view.addSubview(homeMapView)
+//            setConstraints()
+            homeMapView.reloadInputViews()
+            //self.view.addSubview(emptyQuizView)
+            //emptyQuizView.firstQuizTextView.reloadInputViews()
+        }
+    }
 
-    
 
+}
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = homeListView.myTableView.dequeueReusableCell(withIdentifier: "HomeListTableViewCell", for: indexPath) as? HomeListTableViewCell else {return UITableViewCell()}
+        //        cell.textLabel?.text = indexPath.row.description
+        cell.myLabel.text = indexPath.row.description
+        return cell
+    }
+    
+    
 }

e4b7c3a8448d1d70c4dd6740f0c0f86deb3409db
AntonioFlores1 <af513249@gmail.com>
2019-02-11 12:36:58 -0500

    updated UICcollectionView cells and cell population when button is pressed

--- FourSquareReplica/Cells/ListsCollectionViewCell.swift
+++ FourSquareReplica/Cells/ListsCollectionViewCell.swift
@@ -10,4 +10,15 @@ import UIKit
 
 class ListsCollectionViewCell: UICollectionViewCell {
     
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .blue
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }

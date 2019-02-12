2d96e1e570ff48b0df96f85236aa4fc1262138eb
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-02-12 16:15:46 -0500

    created a folder and a model file

---
+++ Model/FourSquareModel.swift
@@ -0,0 +1,9 @@
+//
+//  FourSquareModel.swift
+//  FourSquareReplica
+//
+//  Created by Ibraheem rawlinson on 2/12/19.
+//  Copyright © 2019 Stephanie Ramirez. All rights reserved.
+//
+
+import Foundation
